fb5116d96e1461bef5b764d4ba4e92283af8117b
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2023-12-26 16:44:00 +0000

    Done with category controller.

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -1,27 +1,62 @@
 import { IsArray, IsNotEmpty, IsOptional } from "class-validator"
-import { Body, JsonController, Post } from "routing-controllers"
+import {
+  Body,
+  Delete,
+  Get,
+  JsonController,
+  Param,
+  Post,
+  Put,
+} from "routing-controllers"
 import { Category } from "../models/Category"
+import type { DeepPartial } from "typeorm"
 
-class CategoryCreationBody {
+class CategoryBody {
   @IsNotEmpty()
   // @ts-expect-error doesn't need initialization
   public name: string
 
   @IsArray()
   @IsOptional()
-  // @ts-expect-error doesn't need initialization
-  public keywords: string[] | null
+  public keywords?: string[]
+
+  toCategory(): DeepPartial<Category> {
+    return {
+      name: this.name,
+      keywords: this.keywords ?? [],
+    }
+  }
 }
 
 @JsonController("/categories")
 export class CategoryController {
   @Post("/")
-  async create(@Body() data: CategoryCreationBody): Promise<Category> {
-    const category = Category.create({
-      ...data,
-      keywords: data.keywords ?? [],
-    })
+  async create(@Body() body: CategoryBody): Promise<Category> {
+    return await Category.create(body.toCategory()).save()
+  }
 
-    return await category.save()
+  @Get("/:id")
+  async findOne(@Param("id") id: string): Promise<Category> {
+    return await Category.findOneByOrFail({ id })
+  }
+
+  @Get("/")
+  async find(): Promise<Category[]> {
+    return await Category.find()
+  }
+
+  @Put("/:id")
+  async update(
+    @Param("id") id: string,
+    @Body() body: CategoryBody,
+  ): Promise<Category> {
+    const category = await Category.findOneByOrFail({ id })
+    return await Category.merge(category, body.toCategory()).save()
+  }
+
+  @Delete("/:id")
+  async delete(@Param("id") id: string): Promise<Category> {
+    const category = await Category.findOneByOrFail({ id })
+    return await category.remove()
   }
 }
