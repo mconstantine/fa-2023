980168124418a7707946b43a3dcaa1aa1c75c999
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2024-02-04 23:05:53 +0000

    Created migration and test cases for budget.

---
+++ backend/src/database/functions/budget/domain.ts
@@ -0,0 +1,27 @@
+import * as S from "@effect/schema/Schema"
+import { Category } from "../category/domain"
+
+export const Budget = S.struct({
+  id: S.UUID,
+  year: S.number.pipe(S.int()).pipe(S.positive()),
+  value: S.number.pipe(
+    S.transform(
+      S.number,
+      (n) => parseFloat((n / 100).toFixed(2)),
+      (n) => Math.floor(n * 100),
+    ),
+  ),
+  category_id: S.nullable(S.UUID),
+})
+
+export interface Budget extends S.Schema.To<typeof Budget> {}
+
+export const BudgetWithCategory = S.extend(
+  Budget,
+  S.struct({
+    category: Category,
+  }),
+)
+
+export interface BudgetWithCategory
+  extends S.Schema.To<typeof BudgetWithCategory> {}
---
+++ backend/src/database/tests/budget.test.ts
@@ -0,0 +1,28 @@
+describe("database budget functions", () => {
+  describe("insert budget", () => {
+    it.todo("should work and convert the value")
+  })
+
+  describe("bulk budgets insertion", () => {
+    it.todo("should work and convert the values")
+  })
+
+  describe("update budget", () => {
+    it.todo("should work and convert the value")
+    it.todo("should work with an empty update")
+  })
+
+  describe("bulk budgets update", () => {
+    it.todo("should work and convert the values")
+    it.todo("should work with an empty update")
+  })
+
+  describe("delete budget", () => {
+    it.todo("should work")
+    // TODO: create a test case for categories that tests that budgets are deleted when a category is
+  })
+
+  describe("list budgets", () => {
+    it.todo("should work and filter by year")
+  })
+})
